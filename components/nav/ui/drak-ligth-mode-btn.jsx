273a592aae4b86b1506676b8549c1308e4af3b46
lamani79 <lmutsuha79@gmail.com>
2022-05-19 10:58:39 +0100

    add toggle icon for dark mode sun/moon

--- components/nav/ui/drak-ligth-mode-btn.jsx
+++ components/nav/ui/drak-ligth-mode-btn.jsx
@@ -5,13 +5,15 @@ import { useContext } from "react";
 const DarkLigthModBtn = () => {
   const theme = useContext(ThemeContext);
 
+
   return (
     <div
       onClick={() => {
         theme[1](); // call the function for toggling the theme
       }}
     >
-      <Image src={"/icons/Moon.png"} width={25} height={25} />
+      {theme[0] == 'dark' ? <Image src={"/icons/sun.png"} width={25} height={25} /> : <Image src={"/icons/Moon.png"} width={25} height={25} />}
+      
     </div>
   );
 };
