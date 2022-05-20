c67b1db3f7f26755fb198d0a20f290429928ca0e
lamani79 <lmutsuha79@gmail.com>
2022-05-21 00:18:59 +0100

    fix margin top of hero section in small screans

--- components/hero-section/index.jsx
+++ components/hero-section/index.jsx
@@ -3,7 +3,7 @@ import HeroSlider from "../slider";
 
 const HeroSection = () => {
   return (
-    <section className="mt-8 md:mt-20 lg:w-[80%] xl:w-[70%] mx-auto">
+    <section className="mt-20 lg:w-[80%] xl:w-[70%] mx-auto">
 
       <HeroSlider/>
 
