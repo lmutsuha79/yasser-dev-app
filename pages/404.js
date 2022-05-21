3c546b1f6fe3c6a5bd9b702c0bdc372977a153f6
lamani79 <lmutsuha79@gmail.com>
2022-05-21 23:09:44 +0100

    fix font size for 404 page

--- pages/404.js
+++ pages/404.js
@@ -5,11 +5,11 @@ export default function Custom404() {
   return (
     <div className="container grid grid-cols-1 grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 place-content-center items-center h-screen">
       {/* text */}
-      <div className="text-main-blue font-medium flex flex-col items-center sm:items-start">
-        <h2 className="font-semibold text-6xl sm:text-7xl">404</h2>
-        <span className="text-3xl">Ooops!</span>
-        <span className="text-3xl">Page Not Found</span>
-        <p className="font-normal text-base text-main-gray">
+      <div className="text-main-blue font-medium flex flex-col items-center sm:items-start space-y-2">
+        <h2 className="font-semibold text-6xl sm:text-9xl">404</h2>
+        <span className="text-3xl md:text-5xl">Ooops!</span>
+        <span className="text-3xl md:text-5xl">Page Not Found</span>
+        <p className="font-normal text-base md:text-lg text-main-gray">
           This page doesn't exist or was remooved! I sedgest you back to home
         </p>
 
