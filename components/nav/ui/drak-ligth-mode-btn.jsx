e6a37fb213b4d212f0bf5cae6e69d688fb4d5e71
lamani79 <lmutsuha79@gmail.com>
2022-06-22 18:15:07 +0100

    fix dark_btn size bug

--- components/nav/ui/drak-ligth-mode-btn.jsx
+++ components/nav/ui/drak-ligth-mode-btn.jsx
@@ -2,40 +2,56 @@ import Image from "next/image";
 import { useState, useEffect, useContext } from "react";
 import { ThemeContext } from "../../../pages/_app";
 
-
-
 const DarkLigthModBtn = () => {
   // const [isDark, setIsDark] = useState();
-  const [isOn,setIsOn,changetheme] = useContext(ThemeContext);
- 
-  const toggleDark = () => {
-    const dark = localStorage.getItem('isDark');
-    if(dark === 'false'){
-      return 'false';
-    }
-    else if(dark === 'true'){
-      return 'true';
-    }
-    else{
-      return 'error';
-    }
-  }
+  const [isOn, setIsOn, changetheme] = useContext(ThemeContext);
 
-  
+  const toggleDark = () => {
+    const dark = localStorage.getItem("isDark");
+    if (dark === "false") {
+      return "false";
+    } else if (dark === "true") {
+      return "true";
+    } else {
+      return "error";
+    }
+  };
+
   return (
     <div
       onClick={() => {
-        const prev_is_dark = localStorage.getItem('isDark');
+        const prev_is_dark = localStorage.getItem("isDark");
         changetheme(0);
-        prev_is_dark === 'true' ? setIsOn('false') : setIsOn('true')
-        
-        
+        prev_is_dark === "true" ? setIsOn("false") : setIsOn("true");
       }}
     >
       {isOn === "true" ? (
-        <img src="/icons/sun.webp" alt="btn" width='25px' height='25px'/>
+        <div className="min-w-[25px]">
+          <Image
+            priority="true"
+            src={"/icons/sun.webp"}
+            alt="btn"
+            width={25}
+            height={25}
+            sizes={25}
+            objectFit={"cover"}
+            objectPosition={"center"}
+          />
+        </div>
       ) : (
-        <img src="/icons/Moon.webp" alt="btn" width='25px' height='25px'/>
+        // <img src="/icons/sun.webp" alt="btn" width="25px" height="25px" />
+        <div className="min-w-[25px]">
+          <Image
+            priority="true"
+            src={"/icons/Moon.webp"}
+            alt="btn"
+            width={25}
+            height={25}
+            sizes={25}
+            objectFit={"cover"}
+            objectPosition={"center"}
+          />
+        </div>
       )}
     </div>
   );
