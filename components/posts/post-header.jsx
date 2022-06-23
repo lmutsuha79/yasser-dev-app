08a68c137fea9ec0fe58a8fcac167913fcfc77be
lamani79 <lmutsuha79@gmail.com>
2022-06-23 11:24:32 +0100

    add title to top post intro

--- components/posts/post-header.jsx
+++ components/posts/post-header.jsx
@@ -1,6 +1,6 @@
 import PostTag from "./post-tag";
-import Image from 'next/image'
-const PostHeader = ({meta}) => {
+import Image from "next/image";
+const PostHeader = ({ meta }) => {
   return (
     <div>
       <div className="space-y-2 mb-6">
@@ -18,24 +18,26 @@ const PostHeader = ({meta}) => {
             {meta.date}
           </span>
         </div>
+        <div>
+          <h2 className="text-main-blue text-center text-xl md:text-2xl font-semibold my-2">Intro</h2>
         <p className="font-normal text-base text-main-blue text-center px-3 sm:px-[10%] lg:px-[20%]">
           {meta.intro}
         </p>
+        </div>
       </div>
 
       {/* Post Hero Img  */}
       <div className="text-center max-w-[760px] mx-auto">
         <Image
+          priority={true}
           src={meta.img}
           width={"760"}
           height={"350"}
           sizes={"760"}
           alt={meta.title + " img"}
           objectFit="contain"
-          objectPosition={'center'}
+          objectPosition={"center"}
           draggable="false"
-
-         
         />
       </div>
     </div>
