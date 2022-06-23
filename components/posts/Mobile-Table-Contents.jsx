123788e3e9f8f24d1d256782cf5f6eb79ce7bfff
lamani79 <lmutsuha79@gmail.com>
2022-06-23 11:59:58 +0100

    add hidden class to all break points to make the class work in production mode

--- components/posts/Mobile-Table-Contents.jsx
+++ components/posts/Mobile-Table-Contents.jsx
@@ -6,10 +6,12 @@ import { motion } from "framer-motion";
 const MobileTableContents = ({title, pathName,startHidden }) => {
   const [mbTableIsOpen, setMbTableIsOpen] = useState(false);
   return (
+    
     <div
       onClick={(e) => setMbTableIsOpen((curr) => !curr)}
       className={startHidden+':hidden '+" overflow-hidden h-fit left-2 right-2 fixed  bottom-0 z-[99] drop-shadow-lg border-x border-t-2 border-main-blue-hover/80 transition-all  bg-white  rounded-lg px-4 "}
     >
+      <dvi className='hidden sm:hidden md:hidden lg:hidden xl:hidden 2xl:hidden'></dvi>
       <div className="h-[50px] cursor-pointer text-main-blue font-bold hover:text-main-blue-hover flex justify-between items-center">
         <span>{title ? title : "contents"}</span>
         <motion.div
