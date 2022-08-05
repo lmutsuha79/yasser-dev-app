6cdcdca9ee1974a56cebeb7b05fa0a08e7717f41
lamani79 <lmutsuha79@gmail.com>
2022-08-05 12:04:45 +0100

    refactor the display protfolio items

--- components/Portfolio/works-done/index.jsx
+++ components/Portfolio/works-done/index.jsx
@@ -3,6 +3,72 @@ import UnderLine from "../Under-line";
 import Item from "./item";
 
 const WorksDone = () => {
+  const left_works = [];
+  const right_works = [];
+  const works = [
+    {
+      title: "Magicoon",
+      link: "#",
+      desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
+      imgName: "magicoon-hero.png",
+    },
+    {
+      title: "TO-do Chrom Extention",
+      link: "#",
+      desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
+      imgName: "to_do_chrom_1.jpg",
+    },
+    {
+      title: "Jadoo tailwind Template",
+      link: "#",
+      desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
+      imgName: "travell_tailwind-2.png",
+    },
+    {
+      title: "Doob Template",
+      link: "#",
+      desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
+      imgName: "doob-hero.png",
+    },
+    {
+      title: "Pomodoro PWA with Vue",
+      link: "#",
+      desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
+      imgName: "pomodoro_vue_app-1.webp",
+    },
+    {
+      title: "Type BlogTemplate",
+      link: "#",
+      desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
+      imgName: "type-3.png",
+    },
+  ];
+  works.forEach((item, index) => {
+    if (index === 1 || index % 2 != 0) {
+      right_works.push(
+        <Item
+          key={item.link}
+          link={item.link}
+          title={item.title}
+          desc={item.desc}
+          imgName={item.imgName}
+        />
+      );
+    }
+    else{
+      left_works.push(
+
+        <Item
+          key={item.link}
+          link={item.link}
+          title={item.title}
+          desc={item.desc}
+          imgName={item.imgName}
+        />
+      );
+    }
+  });
+
   return (
     <section className="mt-[100px]">
       <div className="container">
@@ -10,59 +76,14 @@ const WorksDone = () => {
           The work I do, and{" "}
           <UnderLine imgName={"orange.svg"}>business</UnderLine> I help.
         </SectionTitle>
-        
+
         <div className="grid sm:grid-cols-2 gap-x-10 space-y-10">
           <div className="flex flex-col gap-10">
-            <Item
-              link={"#"}
-              title={"Magicoon"}
-              desc={
-                "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!"
-              }
-              imgName={"magicoon-hero.png"}
-            />
-            <Item
-              link={"#"}
-              title={"TO-do Chrom Extention"}
-              desc={
-                "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!"
-              }
-              imgName={"to_do_chrom_1.jpg"}
-            />
-             <Item
-              link={"#"}
-              title={"Jadoo tailwind Template"}
-              desc={
-                "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!"
-              }
-              imgName={"travell_tailwind-2.png"}
-            />
+            {left_works}
+           
           </div>
           <div className="sm:pt-40 flex flex-col gap-10">
-            <Item
-              link={"#"}
-              title={"Doob Template"}
-              desc={
-                "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!"
-              }
-              imgName={"doob-hero.png"}
-            />
-            <Item
-              link={"#"}
-              title={"Pomodoro PWA with Vue"}
-              desc={
-                "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!"
-              }
-              imgName={"pomodoro_vue_app-1.webp"}
-            />
-             <Item
-              link={"#"}
-              title={"Type BlogTemplate"}
-              desc={
-                "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!"
-              }
-              imgName={"type-3.png"}
-            />
+           {right_works}
           </div>
         </div>
       </div>
