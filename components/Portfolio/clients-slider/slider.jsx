048c61ba95aa574a5559642bfa5d76abd2e42f4a
Khelil Yasser Djamel Eddie <74467521+lamani79@users.noreply.github.com>
2022-08-21 15:26:17 +0100

    push last slider portfolio changes

--- components/Portfolio/clients-slider/slider.jsx
+++ components/Portfolio/clients-slider/slider.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import img1 from "../../../public/portfolio/clients/img1.png";
 import img2 from "../../../public/portfolio/clients/img2.png";
+import sayf from "../../../public/portfolio/clients/sayf.png";
 
 import SliderController from "./SliderController";
 const Slider = () => {
@@ -16,13 +17,12 @@ const Slider = () => {
       bgCollor: "#fbe0cb",
     },
     {
-      title:
-        "We've worked with John across all our projects and it's our competitive advantage.",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper, sapien sit amet scelerisque laoreet, eros justo congue neque, in maximus tellus velit vitae lacus. Nam dapibus augue ante, molestie malesuada est ullamcorper vel. Fusce purus ipsum, gravida ut tempus id, aliquam a orci.",
-      name: "Christina Klein",
-      position: "CEO @APPLE",
-      img: img2,
-      bgCollor: "#d0eaeb",
+      title: "Yasser has helped us to made our website called Magiccon",
+      desc: "Thank you very very much for the elaborate and wonderful work that met all my conditions with you at work. The work is executed with great quality and workmanship. I was very pleased to deal with you, and God willing, we will continue soon on other projects",
+      name: "Seyfeddine Beroual",
+      position: "Art Director & Founder at Lamsa Studio",
+      img: sayf,
+      bgCollor: "#a3a3a3",
     },
   ];
 
@@ -70,6 +70,7 @@ const Slider = () => {
             height={470}
             layout="responsive"
             placeholder="blur"
+            draggable="false"
           />
         </div>
       </div>
@@ -81,12 +82,11 @@ const Slider = () => {
         <p className="text-main-blue">{client.desc}</p>
         {/* author */}
         <div className="flex justify-start items-center gap-2">
-          <span className="text-main-blue">{client.name}</span>
+          <span className="text-main-blue font-medium">{client.name}</span>
           <span className="text-main-gray font-medium">{client.position}</span>
         </div>
         <div className="absolute bottom-auto md:bottom-0 right-0">
-        <SliderController goPrev={goPrev} goNext={goNext} />
-
+          <SliderController goPrev={goPrev} goNext={goNext} />
         </div>
       </div>
     </div>
