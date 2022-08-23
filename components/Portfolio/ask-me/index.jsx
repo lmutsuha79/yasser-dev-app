7137787edb029945c6d37df5dff0a58f28f893bd
Khelil Yasser Djamel Eddie <74467521+lamani79@users.noreply.github.com>
2022-08-23 13:03:14 +0100

    add real text on Q and A section

--- components/Portfolio/ask-me/index.jsx
+++ components/Portfolio/ask-me/index.jsx
@@ -3,39 +3,41 @@ import Item from "./item";
 
 const AskMe = () => {
   const QandA = [
-    {
-      question: "What is your work dicipline?",
-      answer:
-        "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.",
-    },
+    // {
+    //   question: "What is your work dicipline?",
+    //   answer:
+    //     "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.",
+    // },
     {
       question: "How often can we communicate?",
       answer:
-        "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.",
+        "We can do it for as long as the business needs to get to you in the best possible way",
     },
     {
       question: "What is your location & timezone?",
       answer:
-        "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.",
+        "I am based in Algeria which is in Central European Standard Time (GMT+1).",
     },
     {
       question: "What is your  hourly rate?",
       answer:
-        "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.",
+        "usually, I work for 15 to 20 dollars per hour it depends on the time zone that I will work in it.",
     },
   ];
   return (
     <section className="mt-[100px] relative z-10">
       <div className="container">
-        <div className="text-center">
+        <div className="text-center ">
           <SectionTitle>
             A few things 🙄
             <br />
             clients normally ask me:
           </SectionTitle>
         </div>
-        <div className="pt-8 space-y-8">
-          {QandA.map((item,index) => <Item key={index} question={item.question} answer={item.answer} />)}
+        <div className="pt-8 space-y-8 ">
+          {QandA.map((item, index) => (
+            <Item key={index} question={item.question} answer={item.answer} />
+          ))}
         </div>
       </div>
     </section>
--- components/Portfolio/footer/index.jsx
+++ components/Portfolio/footer/index.jsx
@@ -15,16 +15,32 @@ const Footer = () => {
         <div className="flex flex-col sm:flex-row gap-2 justify-between items-center border-t border-t-[#f4f6fb] pt-12">
           {/* icons */}
           <div className="flex items-center gap-6 text-main-blue text-xl transition-colors">
-            <a href="#" className={Style.iconItem}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.facebook.com/khelil.yasserdjameledine"
+              className={Style.iconItem}
+            >
               <FontAwesomeIcon icon={faFacebook} />
             </a>
-            <a href="#" className={Style.iconItem}>
+            {/* <a  target="_blank"
+              rel="noreferrer" href="#" className={Style.iconItem}>
               <FontAwesomeIcon icon={faTwitter} />
-            </a>
-            <a href="#" className={Style.iconItem}>
+            </a> */}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.instagram.com/khelil__yasser/"
+              className={Style.iconItem}
+            >
               <FontAwesomeIcon icon={faInstagram} />
             </a>
-            <a href="#" className={Style.iconItem}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/mitsuha79/"
+              className={Style.iconItem}
+            >
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
           </div>
@@ -37,7 +53,7 @@ const Footer = () => {
               className="text-main-blue hover:text-main-blue-hover transition-colors font-medium"
               href="#"
             >
-              yasser-mitsuha@gmail.com
+              khelilyasser79@gmail.com
             </a>
           </div>
         </div>
