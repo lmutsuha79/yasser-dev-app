18f4ff381c5a407637a8220dc7cc84f42e9207c9
Khelil Yasser Djamel Eddie <74467521+lamani79@users.noreply.github.com>
2023-06-30 11:39:47 +0100

    adding jarshop

--- data/worksDone.js
+++ data/worksDone.js
@@ -24,6 +24,13 @@ const works = [
   },
   {
     title: "Like Pro",
+    link: "https://jar-shop.com",
+    desc: "Votre destination unique pour tous vos besoins d'achat",
+    imgName: "jar-shop.png",
+    techs: ["wordpress", "hostinger"],
+  },
+  {
+    title: "Like Pro",
     link: "https://likeproo.com/",
     desc: "صمم خصيصاً لتحصل على افضل اتصال مع جمهورك و لتزيد ولاء عملائك لعلامتك التجارية.",
     imgName: "like_pro.png",
@@ -36,42 +43,42 @@ const works = [
     imgName: "yaser-dev-blog.png",
     techs: [" React/NextJs", "tailwind css", "Mdx"],
   },
-  {
-    title: "Jadoo tailwind Template",
-    link: "https://agitated-euler-c9575e.netlify.app/",
-    desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
-    imgName: "travell_tailwind-2.png",
-    techs: ["Html/Css", "JavaScript", "tailwindCss"],
-  },
-  {
-    title: "Pomodoro PWA with Vue",
-    link: "https://happy-sinoussi-447aef.netlify.app/",
-    desc: "A customizable productivity tool created with Vue JS, featuring focus mode, theme options, and editable work/break intervals for efficient time management and increased productivity.",
-    imgName: "pomodoro_vue_app-1.webp",
-    techs: ["Vue.js", "PWA"],
-  },
-  {
-    title: "TO-do Chrom Extention",
-    link: "https://github.com/lamani79/to_do_list_chrom_widget.git",
-    desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
-    imgName: "to_do_chrom_1.jpg",
-    techs: ["Chrom Extention", "Html/Css", "Sass", "JavaScript"],
-  },
+  // {
+  //   title: "Jadoo tailwind Template",
+  //   link: "https://agitated-euler-c9575e.netlify.app/",
+  //   desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
+  //   imgName: "travell_tailwind-2.png",
+  //   techs: ["Html/Css", "JavaScript", "tailwindCss"],
+  // },
+  // {
+  //   title: "Pomodoro PWA with Vue",
+  //   link: "https://happy-sinoussi-447aef.netlify.app/",
+  //   desc: "A customizable productivity tool created with Vue JS, featuring focus mode, theme options, and editable work/break intervals for efficient time management and increased productivity.",
+  //   imgName: "pomodoro_vue_app-1.webp",
+  //   techs: ["Vue.js", "PWA"],
+  // },
+  // {
+  //   title: "TO-do Chrom Extention",
+  //   link: "https://github.com/lamani79/to_do_list_chrom_widget.git",
+  //   desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
+  //   imgName: "to_do_chrom_1.jpg",
+  //   techs: ["Chrom Extention", "Html/Css", "Sass", "JavaScript"],
+  // },
 
-  {
-    title: "Doob Template",
-    link: "https://lamani79.github.io/Doob_template/",
-    desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
-    imgName: "doob-hero.png",
-    techs: ["Html/Css", "Scss", "JavaScript"],
-  },
+  // {
+  //   title: "Doob Template",
+  //   link: "https://lamani79.github.io/Doob_template/",
+  //   desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
+  //   imgName: "doob-hero.png",
+  //   techs: ["Html/Css", "Scss", "JavaScript"],
+  // },
 
-  {
-    title: "Type BlogTemplate",
-    link: "https://lamani79.github.io/first_bloger_template/",
-    desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
-    imgName: "type-3.png",
-    techs: ["Html/Css", "JavaScript", "Scss"],
-  },
+  // {
+  //   title: "Type BlogTemplate",
+  //   link: "https://lamani79.github.io/first_bloger_template/",
+  //   desc: "Jobriver is a Polish employment platform. It often helps developers to find a job that suits them easily by filtering!",
+  //   imgName: "type-3.png",
+  //   techs: ["Html/Css", "JavaScript", "Scss"],
+  // },
 ];
 export default works;
